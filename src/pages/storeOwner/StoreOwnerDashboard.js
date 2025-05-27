a6be52c29bd5405668a3ff06c857d1ae4a428e8a
win4abhang <win4abhang@gmail.com>
2025-05-27 15:46:38 +0530

    added separate dashboard fixing 1

--- src/pages/storeOwner/StoreOwnerDashboard.js
+++ src/pages/storeOwner/StoreOwnerDashboard.js
@@ -1,7 +1,7 @@
 import React from 'react';
 import { Link, Outlet, useNavigate } from 'react-router-dom';
 
-function StoreDashboard() {
+function StoreOwnerDashboard() {
   const navigate = useNavigate();
 
   const handleLogout = () => {
