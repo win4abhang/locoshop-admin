7c7a75e2eead962984c278a21859e15a90a427cd
win4abhang <win4abhang@gmail.com>
2025-06-02 18:52:43 +0530

    Live

--- src/pages/Register.js
+++ src/pages/Register.js
@@ -13,7 +13,7 @@ import {
 import { useNavigate } from 'react-router-dom';
 import Menu from '../components/Menu';
 
-const cashfree = window.Cashfree({ mode: 'sandbox' });
+const cashfree = window.Cashfree({ mode: 'prduction' });
 
 const BACKEND_URL = 'https://locoshop-backend.onrender.com/api';
 
